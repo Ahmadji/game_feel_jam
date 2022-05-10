590911b3dc6d6f94069931f79e92b80e85680707
Ahmadji <a.ursulet@student.isartdigital.com>
2022-05-10 20:25:00 +0200

    Player Movement v2 (WIP)

--- GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
+++ GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
@@ -8,4 +8,10 @@ public class PlayerData : ScriptableObject
     public float rotationXSpeed;
     [Space, Range(1, 10)]
     public float rotationYSpeed;
+
+    [Header("Player Movement")]
+    public float playerAcceleration = 5f;
+    public float playerJumpForce = 5f;
+    public float brakingAmplitude = 10f;
+    public float StopTime = 3f;
 }
