f285f618fe99ab1e248f956cb0cbff9a60bdd63b
Ahmadji <a.ursulet@student.isartdigital.com>
2022-05-11 11:40:37 +0200

    Continuing Player Movement on Ground
    Fix framerate bug, rework jump, fix player sticking to walls

--- GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
+++ GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
@@ -10,8 +10,16 @@ public class PlayerData : ScriptableObject
     public float rotationYSpeed;
 
     [Header("Player Movement")]
-    public float playerAcceleration = 5f;
-    public float playerJumpForce = 5f;
+    [Tooltip("The higher it is, the faster the player will reach full speed")]
+    public float acceleration = 5f;
+    [Tooltip("Max Speed on the xz plan")]
+    public float maxSpeedXZ = 15f;
+    [Tooltip("Max Speed on the y axis")]
+    public float maxSpeedY = 30f;
+    [Tooltip("Jump Height in meters")]
+    public float jumpHeight = 3f;
+    [Tooltip("Amplitude of the force opposing the movement")]
     public float brakingAmplitude = 10f;
+    [Tooltip("Time to reach full stop after releasing all keys")]
     public float StopTime = 3f;
 }
