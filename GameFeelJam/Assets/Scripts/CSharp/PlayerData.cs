46f938400c1111b079b8c7f81c85db7ccd15e0ea
Ahmadji <a.ursulet@student.isartdigital.com>
2022-05-12 14:37:44 +0200

    Oxygen System + Game Over + Scene Variables

--- GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
+++ GameFeelJam/Assets/Scripts/CSharp/PlayerData.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Player/PlayerData")]
 public class PlayerData : ScriptableObject
@@ -12,6 +14,7 @@ public class PlayerData : ScriptableObject
     [Header("Player State")]
     public bool isGrounded;
     public bool canDash = true;
+    public bool isBreathHolding = false;
 
     [Header("Player Movement")]
     [Tooltip("The higher it is, the faster the player will reach full speed")]
@@ -34,4 +37,26 @@ public class PlayerData : ScriptableObject
     [Header("Player Dash")]
     public float dashAmplitude = 15f;
     public float dashCooldown = 0f;
+
+    [Header("Oxygen System")]
+    public float o2DecreaseBreathing = 0.5f;
+    public float o2DecreaseRunning = 0.25f;
+    public int o2DecreaseDash = 20;
+    public int o2DecreaseJump = 5;
+    public int o2PerCapsules = 20;
+    public float breathHoldingDuration = 10f;
+    [HideInInspector]
+    public List<int> o2Capsules;
+    [HideInInspector]
+    public float durationCooldown = 0.5f;
+    [HideInInspector]
+    public int numMaxCapsules = 5;
+    [HideInInspector]
+    public int subtractRest = 0;
+    [HideInInspector]
+    public int subtractAction = 0;
+    [HideInInspector]
+    public int o2Index = 0;
+    [HideInInspector]
+    public int capsuleLeft = 0;
 }
